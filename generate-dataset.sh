fd46868e238c7d7f4eff6a665d9a86ffd7ea8caf
Matthew Chen <mchen129@umd.edu>
2021-05-26 21:45:19 -0400

    Did part3 part a

---
+++ generate-dataset.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+filename=$1
+num_records=$2
+
+touch "$filename"
+
+for ((i=0; i<$num_records;i++))
+do
+    NUM=$(od -vAn -N4 -tu4 < /dev/urandom) 
+    STRING=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 95 | head -n 1)
+    echo "$NUM $STRING" >> "$filename"
+    echo >> "$filename"
+done
