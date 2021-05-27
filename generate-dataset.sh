93aa75926efa2cbf0647be1ced05ca69a9ef0c3d
Matthew Chen <mchen129@umd.edu>
2021-05-26 22:02:35 -0400

    sorted and unsorted data examples

--- generate-dataset.sh
+++ generate-dataset.sh
@@ -10,5 +10,4 @@ do
     NUM=$(od -vAn -N4 -tu4 < /dev/urandom) 
     STRING=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 95 | head -n 1)
     echo "$NUM $STRING" >> "$filename"
-    echo >> "$filename"
 done
---
+++ sort-data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+filename=$1
+sort -b -no sorted.txt "$filename"
